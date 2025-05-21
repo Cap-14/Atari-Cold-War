82daef2c162de27a10439241e3c4fbdfdea87997
vekidev <93841364+vekidev@users.noreply.github.com>
2025-05-21 23:16:40 +0200

    maybe? meow

--- ACWClient/make.bat
+++ ACWClient/make.bat
@@ -1,4 +1,4 @@
-g++.exe -c main.cpp -Iinclude/
-g++.exe -c AStar.cpp -Iinclude/
-g++.exe -c engine.cpp -Iinclude/
-g++.exe main.o AStar.o engine.o  -o ACW -Llib/ -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -lsfml-network
+g++ -c main.cpp -Iinclude/
+g++ -c AStar.cpp -Iinclude/
+g++ -c engine.cpp -Iinclude/
+g++ main.o AStar.o engine.o  -o ACW -Llib/ -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -lsfml-network
--- ACWServer/make.bat
+++ ACWServer/make.bat
@@ -1,3 +1,3 @@
-g++.exe -c main.cpp -Iinclude/
-g++.exe -c engine.cpp -Iinclude/
-g++.exe main.o engine.o  -o ACW -Llib/ -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -lsfml-network
+g++ -c main.cpp -Iinclude/
+g++ -c engine.cpp -Iinclude/
+g++ main.o engine.o  -o ACW -Llib/ -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -lsfml-network
